3fe84b2f48b8f5b27071b27b9103802c2b8820a2
Richard Vankoningsveld <richyvk@gmail.com>
2017-02-10 22:15:08 +1000

    Add trailing slashes to routes

--- app.py
+++ app.py
@@ -10,37 +10,37 @@ def index():
 
 
 # birthday route
-@app.route('/birthday')
+@app.route('/birthday/')
 def bday():
     return '1 January 1970'
 
 
 # hello route with variable name
-@app.route('/greeting/<name>')
+@app.route('/greeting/<name>/')
 def greet(name):
     return 'Hello, {0}!'.format(name)
 
 
 # sum route
-@app.route('/sum/<int:a>/<int:b>')
+@app.route('/sum/<int:a>/<int:b>/')
 def sum_ints(a, b):
     return "The sum of {0} and {1} is {2}.".format(a, b, a + b)
 
 
 # multiply route
-@app.route('/multiply/<int:a>/<int:b>')
+@app.route('/multiply/<int:a>/<int:b>/')
 def multiply(a, b):
     return "{0} mulitplied by {1} is {2}.".format(a, b, a * b)
 
 
 # subtract route
-@app.route('/subtract/<int:a>/<int:b>')
+@app.route('/subtract/<int:a>/<int:b>/')
 def subtract(a, b):
     return "{0} minus {1} is {2}.".format(a, b, a - b)
 
 
 # favourite foods route
-@app.route('/favoritefoods')
+@app.route('/favoritefoods/')
 def fav_foods():
     foods = ['cheese', 'bacon', 'apples', 'bread', 'more cheese']
     return jsonify(foods)
